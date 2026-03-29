7644c4e4739e6e4859a3e916e0ed7eed3c26a219
Rojan Kayastha <rojankayastha59@gmail.com>
2026-03-29 09:12:54 +0545

    updated users's url routes

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    
+     # Auth
+    path('register/',       views.RegisterView.as_view(),  name='register'),
+    path('login/',          views.LoginView.as_view(),      name='login'),
+    path('token/refresh/',  TokenRefreshView.as_view(),     name='token-refresh'),
+
+    #Users
     path('users/', views.UserList.as_view(), name="user-list"),
     path('users/<int:pk>',views.UserDetail.as_view(), name="user-detail"),
 ]
